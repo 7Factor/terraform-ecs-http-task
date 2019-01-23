dd99fe3b055e0d6118023c8b0cb5d41731ab2c42
jduv <jduv@7factor.io>
2019-01-23 14:55:47 -0500

    Adding health checks with sane defaults

--- lb.tf
+++ lb.tf
@@ -5,7 +5,7 @@ resource "aws_lb" "app_lb" {
   security_groups    = ["${var.cluster_lb_sg_id}"]
 
   tags {
-    Name = "${var.app_name} LB"
+    Name = "Application LB (${var.app_name})"
   }
 }
 
@@ -45,6 +45,17 @@ resource "aws_lb_target_group" "lb_targets" {
   vpc_id               = "${var.vpc_id}"
   target_type          = "instance"
   deregistration_delay = "60"
+
+  health_check {
+    interval            = "${var.health_check_interval}"
+    path                = "${var.health_check_path}"
+    port                = "${var.health_check_port}"
+    protocol            = "${var.health_check_protocol}"
+    timeout             = "${var.health_check_timeout}"
+    healthy_threshold   = "${var.health_check_threshold}"
+    unhealthy_threshold = "${var.health_check_unhealthy_threshold}"
+    matcher             = "${var.health_check_matcher}"
+  }
 
   lifecycle {
     create_before_destroy = true
