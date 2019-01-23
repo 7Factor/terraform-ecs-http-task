13b959db24515ff6d22c4cba9d824635ababe3ee
jduv <jduv@7factor.io>
2019-01-23 14:55:47 -0500

    Adding health checks with sane defaults

--- variables.tf
+++ variables.tf
@@ -31,6 +31,47 @@ variable "cluster_lb_sg_id" {
   description = "The id of the ECS cluster load balancer security group."
 }
 
+// Health check (defaults to something sane)
+variable "health_check_interval" {
+  default     = 30
+  description = "The approximate amount of time, in seconds, between health checks of an individual target. Minimum value 5 seconds, Maximum value 300 seconds. Default 30 seconds."
+}
+
+variable "health_check_path" {
+  default     = "/"
+  description = "The destination for the health check request."
+}
+
+variable "health_check_port" {
+  default     = "traffic-port"
+  description = "The port to use to connect with the target. Valid values are either ports 1-65536, or traffic-port. Defaults to traffic-port."
+}
+
+variable "health_check_protocol" {
+  default     = "HTTP"
+  description = "The protocol to use to connect with the target. Defaults to HTTP."
+}
+
+variable "health_check_timeout" {
+  default     = 5
+  description = "The amount of time, in seconds, during which no response means a failed health check. For Application Load Balancers, the range is 2 to 60 seconds and the default is 5 seconds. "
+}
+
+variable "health_check_threshold" {
+  default     = 3
+  description = "The number of consecutive health checks successes required before considering an unhealthy target healthy. Defaults to 3."
+}
+
+variable "health_check_unhealthy_threshold" {
+  default     = 3
+  description = "The number of consecutive health check failures required before considering the target unhealthy ."
+}
+
+variable "health_check_matcher" {
+  default     = "200"
+  description = "The HTTP codes to use when checking for a successful response from a target. You can specify multiple values (for example, '200,202') or a range of values (for example, '200-299'). "
+}
+
 // Task configuration
 variable "app_name" {
   description = "The name of your app. This is used in the task configuration."
