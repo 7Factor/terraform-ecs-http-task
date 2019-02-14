dfa1f6ef8c23b3df157999e3de375f31f4a08266
chris-malloy <chrismalloymusic@gmail.com>
2019-02-14 14:32:10 -0500

    Adding health check path to outputs.

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "lb_hostname" {
   value = "${aws_lb.app_lb.dns_name}"
 }
 
+output "health_check_path" {
+  value = "${aws_lb_target_group.lb_targets.health_check.path}"
+}
+
 output "lb_zone_id" {
   value = "${aws_lb.app_lb.zone_id}"
 }
