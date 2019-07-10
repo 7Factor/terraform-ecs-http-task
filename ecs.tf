d3e0faae5e232fe2d9ea999da65749ed75714c65
chris-malloy <chrismalloymusic@gmail.com>
2019-07-10 17:54:04 -0400

    Adding support for multiple volume blocks, this is compatible with terraform 0.12.

--- ecs.tf
+++ ecs.tf
@@ -12,9 +12,13 @@ resource "aws_ecs_task_definition" "main_task" {
 
   task_role_arn = var.task_role_arn
 
-  volume {
-    name = var.volume_name
-    host_path = var.volume_host_path
+  dynamic "volume" {
+    for_each = var.volume_blocks
+
+    content {
+      name      = volume_name.value
+      host_path = volume_host_path.value
+    }
   }
 }
 
