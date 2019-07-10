eea5aa5a43867758cb12fffe5d2b96dd07e1458d
chris-malloy <chrismalloymusic@gmail.com>
2019-07-10 09:56:49 -0400

    Converting to HCL2 expressions.

--- ecs.tf
+++ ecs.tf
@@ -1,35 +1,35 @@
 data "aws_ecs_cluster" "target_cluster" {
-  cluster_name = "${var.cluster_name}"
+  cluster_name = var.cluster_name
 }
 
 resource "aws_ecs_task_definition" "main_task" {
   family                   = "${var.app_name}-tsk"
   requires_compatibilities = ["EC2"]
   network_mode             = "bridge"
-  cpu                      = "${var.cpu}"
-  memory                   = "${var.memory}"
-  container_definitions    = "${var.container_definition}"
+  cpu                      = var.cpu
+  memory                   = var.memory
+  container_definitions    = var.container_definition
 
-  task_role_arn = "${var.task_role_arn}"
+  task_role_arn = var.task_role_arn
 
   volume {
-    name = "${var.volume}"
+    name = var.volume
   }
 }
 
 resource "aws_ecs_service" "main_service" {
   name                       = "${var.app_name}-svc"
-  task_definition            = "${aws_ecs_task_definition.main_task.arn}"
-  cluster                    = "${data.aws_ecs_cluster.target_cluster.id}"
-  desired_count              = "${var.desired_task_count}"
-  iam_role                   = "${var.service_role_arn}"
-  launch_type                = "${var.launch_type}"
-  deployment_maximum_percent = "${var.service_deployment_maximum_percent}"
+  task_definition            = aws_ecs_task_definition.main_task.arn
+  cluster                    = data.aws_ecs_cluster.target_cluster.id
+  desired_count              = var.desired_task_count
+  iam_role                   = var.service_role_arn
+  launch_type                = var.launch_type
+  deployment_maximum_percent = var.service_deployment_maximum_percent
 
   load_balancer {
-    container_name   = "${var.app_name}"
-    container_port   = "${var.app_port}"
-    target_group_arn = "${aws_lb_target_group.lb_targets.arn}"
+    container_name   = var.app_name
+    container_port   = var.app_port
+    target_group_arn = aws_lb_target_group.lb_targets.arn
   }
 
   depends_on = ["aws_lb.app_lb"]
