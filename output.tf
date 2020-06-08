b056ae4a52851a89cf8d6ec0ecdf36161c7f21a0
chris-malloy <chrismalloymusic@gmail.com>
2020-06-08 15:19:50 -0400

    Testing ecs configuration.

--- output.tf
+++ output.tf
@@ -9,3 +9,8 @@ output "health_check_path" {
 output "lb_zone_id" {
   value = aws_lb.app_lb.zone_id
 }
+
+output "task_definition_arn" {
+  value       = aws_ecs_task_definition.main_task.arn
+  description = "The arn of your task definition"
+}
